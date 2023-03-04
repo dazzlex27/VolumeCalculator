102bc878f970325292b0917539db10f47d57f636
Alexey Ponomarenko <avponomarenko94@gmail.com>
2023-03-04 21:32:52 +0100

    Fixed the line counting script.
    Former-commit-id: 98d5bc64fb6450d8f57ee6b3b464b184fd5ed7a5
    Former-commit-id: d920bf80227fdce0770019b2fdf50e5e850376a3

--- Tools/total_line_count.py
+++ Tools/total_line_count.py
@@ -1,40 +1,25 @@
 import os
 import traceback
 
-dir_list = (
-"D435FrameProvider",
-"DeviceIntegration",
-"ExtIntegration",
-"FrameProviders",
-"FrameProcessor",
-"libDepthMapProcessor",
-"Primitives",
-"Tests/VolumeCalculatorTests", 
-"Tests/DepthMapProcessorTests", 
-"Tests/VolumeCalculatorTests",
-"Utils/MacReader",
-"Utils/VersionWriter",
-"VolumeCalculatorGUI")
+root_dir = '../'
+excluded_dirs = ['.git', '.vs']
 ext_list = ("h", "cpp", "c", "cs")
 
 total_count = 0
 
-print("Line count by project:")
-print("===============")
 try:
-    for directory in dir_list:
-        count = 0
-        for (dir,_, files) in os.walk(os.path.join("..", directory)):
-                for f in files:
-                    path = os.path.join(dir, f)
+    for (dir,_, files) in os.walk(root_dir):
+        contains_exc_dir = any(ed in dir for ed in excluded_dirs)
+        if contains_exc_dir:
+            continue
+            
+        for f in files:
+            path = os.path.join(dir, f)
 
-                    file_is_ok = os.path.exists(path) and path.endswith(ext_list)
-                    file_is_service = path.endswith("g.i.cs") or "\\obj\\" in path
-                    if file_is_ok and not file_is_service:
-                        count += len(open(path, "rt", encoding="utf-8").readlines(  ))
-        total_count += count
-        print(directory + ": ", count)
-    print("===============")
+            file_is_ok = os.path.exists(path) and path.endswith(ext_list)
+            file_is_service = path.endswith("g.i.cs") or "\\obj\\" in path
+            if file_is_ok and not file_is_service:
+                total_count += len(open(path, "rt", encoding="utf-8").readlines(  ))
     print("Total count = ", total_count)
 except:
     traceback.print_exc()
